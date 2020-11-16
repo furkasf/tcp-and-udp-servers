cdf2b8eb687be0774111f4b1edbb4dd82c82a960
furkan tural <furkantural3328@hotmail.com>
2020-11-16 22:38:18 +0300

    tcp server done

---
+++ tcp.js
@@ -0,0 +1,8 @@
+
+const net = require('http')
+const server = net.createServer(socket => {
+    socket.write('hello')
+    socket.on('data', data => console.log(data.toString()))
+})
+
+server.listen(8080)
